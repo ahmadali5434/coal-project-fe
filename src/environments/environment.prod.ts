f3de968dc1ad71209a91eb816e87fb3dbeec825d
shahidedu <s-ahmad@edu.com.sa>
2025-09-12 01:10:56 +0500

    remove comments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://coal-project-be-production.up.railway.app/api',// Replace with your production API URL
+  apiBaseUrl: 'https://coal-project-be-production.up.railway.app/api',
 }
